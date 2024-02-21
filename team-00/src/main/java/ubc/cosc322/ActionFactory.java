2e335e1a3db6886cae5b05bb1e7068353d5bbf21
Mackenzie Kudrenecky <mkudrene@student.ubc.ca>
2024-02-21 12:55:59 -0800

    create Queen and Board classes

--- team-00/src/main/java/ubc/cosc322/ActionFactory.java
+++ team-00/src/main/java/ubc/cosc322/ActionFactory.java
@@ -2,9 +2,17 @@ package ubc.cosc322;
 import java.util.ArrayList;
 
 public class ActionFactory {
+    
 
-    public ArrayList<Action> getActions(){
+    public ArrayList<Action> getActions(Board board, Player player){
         ArrayList<Action> actions = new ArrayList<>();
+        // Iterate through all queens controlled by the player
+
+        // Generate queen moves for the current queen
+
+        // Generate arrow placements for each queen move
+
+        // Create actions for each combination of queen move and arrow placement
         return actions;
     }
 }
---
+++ team-00/src/main/java/ubc/cosc322/Board.java
@@ -0,0 +1,10 @@
+package ubc.cosc322;
+import java.util.Map;
+
+public class Board {
+
+    public Board (Map<String, Object> state){
+
+    }
+    
+}
